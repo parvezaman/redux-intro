8b8c1a7259c4902e0a2020fd2a2e4b470a9407fb
parvezaman <emailtoamanullah@gmail.com>
2023-02-02 21:55:17 +0600

    initial folder stracture for resux and dont forget to assign the initial state to the reducer state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
@@ -13,7 +14,9 @@ const Home = () => {
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+      {
+        products.map(product => <ProductCard key={product.name} product={product} />)
+      }
     </div>
   );
 };
---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,7 @@
+const initialState = {};
+
+const productReducer = (state = initialState, action) => {
+
+}
+
+export default productReducer;
