fbe313873b410ccda1f62362ecc2763bceb9ca01
parvezaman <emailtoamanullah@gmail.com>
2023-02-11 21:13:42 +0600

    inital state is provided into reducer, store created and connected to the reducer, provided store globally, consumed the state where we need

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const state = useSelector((state)=> state)
 
   useEffect(() => {
     fetch('products.json')
@@ -10,7 +12,7 @@ const Home = () => {
       .then((data) => setProducts(data))
   }, [])
 
-  console.log(products);
+  console.log(state);
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,7 +1,9 @@
-const initialState = {};
+const initialState = {
+    test: "test text"
+};
 
 const productReducer = (state = initialState, action) => {
-
+    return state;
 }
 
 export default productReducer;
