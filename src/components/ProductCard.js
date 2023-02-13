f16cd255c6b2770d7efb35127039d7bdd3514bd2
parvezaman <emailtoamanullah@gmail.com>
2023-02-13 11:07:11 +0600

    add to cart will increase quantity for existing product or add a new product

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -10,9 +10,12 @@ const ProductCard = ({ product }) => {
 
   return (
     <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
+      className='relative shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
       key={product._id}
     >
+      {pathname.includes('cart') && <div className="grid place-items-center bg-indigo-500 text-white h-8 w-8 rounded-full absolute top-0 right-0">
+        <p>{product.quantity}</p>
+      </div>}
       <div className='h-52 w-52 mx-auto'>
         <img src={product.image} alt={product.model} />
       </div>
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -5,10 +5,20 @@ const initialState = {
 };
 
 const productReducer = (state = initialState, action) => {
+    const selectedProduct = state.cart.find(product => product.model === action.payload.model)
+    console.log(selectedProduct);
     switch (action.type) {
         case actionTypes.ADD_TO_CART:
+            if (selectedProduct) {
+                const cartWithoutNewItem = state.cart.filter(product => product.model !== action.payload.model)
+                selectedProduct.quantity++;
+                return {
+                    ...state, cart: [...cartWithoutNewItem, selectedProduct]
+                };
+            }
             return {
-                ...state, cart: [...state.cart, action.payload]
+                ...state,
+                cart: [...state.cart, { ...action.payload, quantity: 1 }]
             };
         case actionTypes.REMOVE_FROM_CART:
             return {
